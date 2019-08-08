02f3ba37296b750f6c2afe7e12ca8bff61af81bd
Polyphemus487 <52760437+Polyphemus487@users.noreply.github.com>
2019-08-08 15:32:45 -0700

    Add files via upload

---
+++ run-models.py
@@ -0,0 +1,14 @@
+import os
+
+models = ['VGG19', 'MobileNetV3_Large', 'SqueezeNet1.0', 'AlexNet', 'darknet53', 'InceptionV3', 'Xception']
+
+
+def run_through_models():
+    for model in models:
+        print(model)
+        os.system(f'python imagenet_multi_generalized.py --model {model} --input-fldr /Users/sam/imagenet' \
+                  +f'-vid-robust-example-master/imagenet-vid-robust/val --save-to-file /Users/sam/save-results/{model}'\
+                  +'_full --display-in-terminal False')
+
+
+run_through_models()
